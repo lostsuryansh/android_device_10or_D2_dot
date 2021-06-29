42eb199974be645b46442fde34eb0d25efdfe8c4
lostsuryansh <84130468+lostsuryansh@users.noreply.github.com>
2021-06-29 09:02:36 +0530

    Create aosp_D2.mk
    Fix Dot

--- aosp_D2.mk
+++ aosp_D2.mk
@@ -18,10 +18,10 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/aosp/common.mk)
+$(call inherit-product, vendor/dot/common.mk)
 
 # Inherit from D2 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -29,7 +29,6 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 # Boot Animation
 TARGET_BOOT_ANIMATION_RES := 720
 
-EXTENDED_BUILD_TYPE := UNOFFICIAL
 PRODUCT_BRAND := 10or
 PRODUCT_DEVICE := D2
 PRODUCT_MANUFACTURER := 10or
